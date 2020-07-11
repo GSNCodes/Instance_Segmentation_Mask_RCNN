adb13ceb45b0d5aac253db420b582c1994f4b310
GSNCodes <gsnspidey@gmail.com>
2020-07-11 18:53:47 +0530

    Added more options

--- mask.py
+++ mask.py
@@ -12,18 +12,14 @@ import matplotlib
 
 
 class MyConfig(CocoConfig):
+    NAME = "my_coco_inference"
+    # Set batch size to 1 since we'll be running inference on one image at a time.
+    # Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
 
-        NAME = "my_coco_inference"
-        # Set batch size to 1 since we'll be running inference on one image at a time.
-        # Batch size = GPU_COUNT * IMAGES_PER_GPU
-        GPU_COUNT = 1
-        IMAGES_PER_GPU = 1
-
-
-    
 
 def prepare_mrcnn_model(model_path, model_name, class_names, my_config):
-
     classes = open(class_names).read().strip().split("\n")
     print("No. of classes", len(classes))
 
@@ -38,42 +34,50 @@ def prepare_mrcnn_model(model_path, model_name, class_names, my_config):
     return COLORS, model, classes
 
 
-def perform_inference_image(image_path, model, colors, classes, draw_bbox, mrcnn_visualize):
+def perform_inference_image(image_path, model, colors, classes, draw_bbox, mrcnn_visualize, instance_segmentation,
+                            save_enable):
     test_image = cv2.imread(image_path)
     test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB)
 
-    output = custom_visualize(test_image, model, colors, classes, draw_bbox, mrcnn_visualize)
+    output = custom_visualize(test_image, model, colors, classes, draw_bbox, mrcnn_visualize, instance_segmentation)
+    if not mrcnn_visualize:
+        if save_enable:
+            cv2.imwrite("result.png", output)
+        cv2.imshow("Output", output)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
-    cv2.imwrite("result.png", output)
-    cv2.imshow("Output", output)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
 
-
-def custom_visualize(test_image, model, colors, classes, draw_bbox , mrcnn_visualize):
-
+def custom_visualize(test_image, model, colors, classes, draw_bbox, mrcnn_visualize, instance_segmentation):
     detections = model.detect([test_image], verbose=1)[0]
-
 
     if mrcnn_visualize:
         matplotlib.use('TkAgg')
-        visualize.display_instances(test_image, detections['rois'], detections['masks'], detections['class_ids'], classes,
-                                detections['scores'])
+        visualize.display_instances(test_image, detections['rois'], detections['masks'], detections['class_ids'],
+                                    classes,
+                                    detections['scores'])
 
         return
+
+    if instance_segmentation:
+        hsv = [(i / len(detections['rois']), 1, 1.0) for i in range(len(detections['rois']))]
+        colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+        random.seed(42)
+        random.shuffle(colors)
 
     for i in range(0, detections["rois"].shape[0]):
         classID = detections["class_ids"][i]
 
         mask = detections["masks"][:, :, i]
-        color = colors[classID][::-1]
+        if instance_segmentation:
+            color = colors[i][::-1]
+        else:
+            color = colors[classID][::-1]
 
         # To visualize the pixel-wise mask of the object
         test_image = visualize.apply_mask(test_image, mask, color, alpha=0.5)
 
-
     test_image = cv2.cvtColor(test_image, cv2.COLOR_RGB2BGR)
-
 
     if draw_bbox:
         for i in range(0, len(detections["scores"])):
@@ -82,7 +86,12 @@ def custom_visualize(test_image, model, colors, classes, draw_bbox , mrcnn_visua
             classID = detections["class_ids"][i]
             label = classes[classID]
             score = detections["scores"][i]
-            color = [int(c) for c in np.array(colors[classID]) * 255]
+
+            if instance_segmentation:
+                color = [int(c) for c in np.array(colors[i]) * 255]
+
+            else:
+                color = [int(c) for c in np.array(colors[classID]) * 255]
 
             cv2.rectangle(test_image, (startX, startY), (endX, endY), color, 2)
             text = "{}: {:.2f}".format(label, score)
@@ -92,21 +101,34 @@ def custom_visualize(test_image, model, colors, classes, draw_bbox , mrcnn_visua
     return test_image
 
 
-def perform_inference_video(use_camera,video_path,model, colors, classes, draw_bbox, mrcnn_visualize):
+def perform_inference_video(use_camera, video_path, model, colors, classes, draw_bbox, mrcnn_visualize,
+                            instance_segmentation, save_enable):
     if use_camera:
         video = cv2.VideoCapture(0)
         time.sleep(2.0)
     else:
         video = cv2.VideoCapture(video_path)
 
+    video_flag = True
     while True:
         ret, frame = video.read()
 
+        if save_enable and video_flag:
+            out = cv2.VideoWriter("Result_Video.mp4", cv2.VideoWriter_fourcc(*'MP4V'), 8,
+                                  (frame.shape[1], frame.shape[0]))
+            video_flag = False
+
+        if not ret:
+            break
+
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        output = custom_visualize(frame, model, colors, classes, draw_bbox, mrcnn_visualize)
+        output = custom_visualize(frame, model, colors, classes, draw_bbox, mrcnn_visualize, instance_segmentation)
 
         cv2.imshow("Output", output)
+
+        if save_enable:
+            out.write(output)
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
@@ -115,34 +137,38 @@ def perform_inference_video(use_camera,video_path,model, colors, classes, draw_b
     video.release()
 
 
+if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', help='Path to the test images', default=None)
+    parser.add_argument('--model_path', help='Path to the model directory', default='models/')
+    parser.add_argument('--model_name', help='Name of the model file', default='models/mask_rcnn_coco.h5')
+    parser.add_argument('--class_names', help='Path to the class labels', default='coco_classes.txt')
+    parser.add_argument('--mrcnn_visualize', help='Use the built-in visualize method', type=bool, default=False)
+    parser.add_argument('--instance_segmentation', help='To toggle between semantic and instance segmentation',
+                        type=bool, default=True)
+    parser.add_argument('--draw_bbox', help='Draw the bounding box with class labels', type=bool, default=True)
+    parser.add_argument('--camera', help='Perform live detection', type=bool, default=False)
+    parser.add_argument('--video', help='Path to video file', default=None)
+    parser.add_argument('--save_enable', help='Enable to save processed image or video', type=bool, default=False)
+    args = vars(parser.parse_args())
 
+    if args['image']:
+        my_config = MyConfig()
+        my_config.display()
+        colors, model, classes = prepare_mrcnn_model(args['model_path'], args['model_name'], args['class_names'],
+                                                     my_config)
+        perform_inference_image(args['image'], model, colors, classes, args['draw_bbox'], args['mrcnn_visualize'],
+                                args['instance_segmentation'], args['save_enable'])
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--image', help='Path to the test images', default=None)
-parser.add_argument('--model_path', help='Path to the model directory', default='models/')
-parser.add_argument('--model_name', help='Name of the model file', default='models/mask_rcnn_coco.h5')
-parser.add_argument('--class_names', help='Path to the class labels', default='coco_classes.txt')
-parser.add_argument('--mrcnn_visualize', help='Use the built-in visualize method', type=bool, default=False)
-parser.add_argument('--draw_bbox', help='Draw the bounding box with class labels', type=bool, default=True)
-parser.add_argument('--camera', help='Perform live detection', type=bool, default=False)
-parser.add_argument('--video', help='Path to video file', default=None)
-args = vars(parser.parse_args())
+    if args['camera'] or args['video']:
+        use_camera = args['camera']
+        video_path = args['video']
 
-
-if args['image']:
-    my_config = MyConfig()
-    my_config.display()
-    colors, model, classes = prepare_mrcnn_model(args['model_path'], args['model_name'], args['class_names'], my_config)
-    perform_inference_image(args['image'], model, colors, classes, args['draw_bbox'], args['mrcnn_visualize'])
-    
-
-if args['camera'] or args['video']:
-
-    use_camera = args['camera']
-    video_path = args['video']
-
-    my_config = MyConfig()
-    my_config.display()
-    colors, model, classes = prepare_mrcnn_model(args['model_path'], args['model_name'], args['class_names'], my_config)
-    perform_inference_video(use_camera, video_path, model, colors, classes, args['draw_bbox'], args['mrcnn_visualize'])
+        my_config = MyConfig()
+        my_config.display()
+        colors, model, classes = prepare_mrcnn_model(args['model_path'], args['model_name'], args['class_names'],
+                                                     my_config)
+        perform_inference_video(use_camera, video_path, model, colors, classes, args['draw_bbox'],
+                                args['mrcnn_visualize'],
+                                args['instance_segmentation'], args['save_enable'])
